6c9a6e17b4d6255da97c3d8ac3c573540a99226d
roku36 <roku36000@gmail.com>
2023-12-15 17:48:52 +0900

    add whick-key of tt, tn

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "tt", "<cmd>ObsidianToday<CR>")
+vim.keymap.set("n", "tt", "<cmd>ObsidianToday<CR>", { desc = "Obsidian Today" })
+vim.keymap.set("n", "tn", "<cmd>ObsidianNew<CR>", { desc = "Obsidian New" })
--- init.lua
+++ init.lua
@@ -564,6 +564,7 @@ require('which-key').register {
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  ['t'] = { name = '[T]oday', _ = 'which_key_ignore' },
 }
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
