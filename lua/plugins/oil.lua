af5f76fc63268fb792d9cd1196330bde4cba719a
roku36 <roku36000@gmail.com>
2023-12-23 14:07:52 +0900

    fix indent to 2

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,14 @@
-return {
-	'stevearc/oil.nvim',
+return
+{
+	"stevearc/oil.nvim",
 	opts = {},
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			win_options = {
+				cursorcolumn = true,
+			}
+		})
+	end,
 }
