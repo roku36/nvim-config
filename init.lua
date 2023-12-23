df6a28a7a4c5db85c37ee233d882acea90fe893c
roku36 <roku36000@gmail.com>
2023-12-23 12:35:23 +0900

    disable tab to select cmp

--- init.lua
+++ init.lua
@@ -649,8 +649,20 @@ cmp.setup {
     completeopt = 'menu,menuone,noselect',
   },
   mapping = cmp.mapping.preset.insert {
-    ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<C-k>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
@@ -659,18 +671,14 @@ cmp.setup {
       select = false,
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
+      if luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       else
         fallback()
       end
     end, { 'i', 's' }),
     ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
+      if luasnip.locally_jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
--- lua/plugins/skk.lua
+++ lua/plugins/skk.lua
@@ -1,9 +1,9 @@
 return {
-  "vim-skk/skkeleton",
-  dependencies = {
-    "vim-denops/denops.vim",
-  },
-  config = function()
-    vim.keymap.set("i", "<C-j>", "<Plug>(skkeleton-toggle)")
-  end,
+  -- "vim-skk/skkeleton",
+  -- dependencies = {
+  --   "vim-denops/denops.vim",
+  -- },
+  -- config = function()
+  --   vim.keymap.set("i", "<C-j>", "<Plug>(skkeleton-toggle)")
+  -- end,
 }
