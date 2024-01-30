f4a2c750eb403bd1fb7989ffaf35ca40cde79ea2
roku36 <roku36000@gmail.com>
2024-01-30 20:19:47 +0900

    update plugins

---
+++ lua/plugins/cpp.lua
@@ -0,0 +1,20 @@
+return {
+  -- "ranjithshegde/ccls.nvim",
+  -- config = function()
+  --   local util = require "lspconfig.util"
+  --   local server_config = {
+  --       filetypes = { "c", "cpp", "objc", "objcpp", "opencl" },
+  --       root_dir = function(fname)
+  --           return util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname)
+  --               or util.find_git_ancestor(fname)
+  --       end,
+  --       init_options = { cache = {
+  --           -- directory = vim.env.XDG_CACHE_HOME .. "/ccls/",
+  --           vim.fs.normalize "~/.cache/ccls" -- if on nvim 0.8 or higher
+  --       } },
+  --       --on_attach = require("my.attach").func,
+  --       --capabilities = my_caps_table_or_func
+  --   }
+  --   require("ccls").setup { lsp = { lspconfig = server_config } }
+  -- end
+}
