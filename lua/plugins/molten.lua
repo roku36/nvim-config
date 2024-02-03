078fbc90e4b537b1929049b22782d6c9878a53c6
roku36 <roku36000@gmail.com>
2024-02-03 23:39:49 +0900

    use image.nvim to output cell result

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -8,16 +8,26 @@ return {
     build = ":UpdateRemotePlugins",
     init = function()
       -- this is an example, not a default. Please see the readme for more configuration options
-      vim.g.molten_output_win_max_height = 12
+      -- vim.g.molten_output_win_max_height = 12
+      vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_use_border_highlights = true
+      
+      -- change color depending on status
+      -- vim.api.nvim_set_hl(0, "MoltenOutputBorder", { fg = "blue", bg = "black" })
+      -- vim.api.nvim_set_hl(0, "MoltenOutputBorderFail", { fg = "red", bg = "black" })
+      -- vim.api.nvim_set_hl(0, "MoltenOutputBorderSuccess", { fg = "green", bg = "black" })
+      -- vim.g.molten_output_virt_lines = true
+
       -- vim.g.python3_host_prog = '/home/roku/.rye/shims/python3'
       -- vim.g.python3_host_prog = '/Users/roku/lab/jaaaax/.venv/bin/python3'
       -- vim.g.python3_host_prog = "/Users/roku/lab/marljax/.venv/bin/python"
 
       vim.keymap.set("n", "<localleader><localleader>mi", ":MoltenInit<CR>")
-      vim.keymap.set("n", "<localleader><localleader>me", ":MoltenEvaluateOperator<CR>")
-      vim.keymap.set("n", "<localleader><localleader>mr", ":MoltenReevaluateCell<CR>")
-      vim.keymap.set("v", "<localleader><localleader>mv", ":<C-u>MoltenEvaluateVisual<CR>gv")
-      vim.keymap.set("n", "<localleader><localleader>ms", ":noautocmd MoltenEnterOutput<CR>")
+      -- vim.keymap.set("n", "<localleader>M", ":MoltenEvaluateOperator<CR>")
+      vim.keymap.set("n", "<localleader><S-m>", ":MoltenReevaluateCell<CR>")
+      -- vim.keymap.set("v", "<localleader><localleader>mv", ":<C-u>MoltenEvaluateVisual<CR>gv")
+      vim.keymap.set("v", "<localleader>m", ":<C-u>MoltenEvaluateVisual<CR>gv")
+      vim.keymap.set("n", "<localleader><S-k>", ":noautocmd MoltenEnterOutput<CR>")
       vim.keymap.set("n", "<localleader><localleader>mh", ":MoltenHideOutput<CR>")
       vim.keymap.set("n", "<localleader><localleader>md", ":MoltenDelete<CR>")
     end,
